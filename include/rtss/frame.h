788658c9f36a069af44c00650ef19830019dfe32
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-12-10 21:14:46 +0300

    Frame implementation. Not perfect, intended to be improved later.

--- include/rtss/frame.h
+++ include/rtss/frame.h
@@ -11,26 +11,75 @@ namespace rtss {
             : task_id(task_id), exec_tm(exec_tm) {
         }
 
+        [[nodiscard]] std::string to_string() const {
+            std::ostringstream oss;
+            oss << "{T" << task_id << ", " << time::toInt(exec_tm) << "ms}";
+            return oss.str();
+        }
+
         int16_t task_id;
         time::TimeDuration exec_tm;
     };
 
     class Frame {
     public:
-        explicit Frame(const std::vector<Task *> &tasks_ref)
-            : _tasks_ref(const_cast<std::vector<Task *> &>(tasks_ref)) {
+        explicit Frame(const std::vector<FrameJob> &&jobs)
+            : _jobs(jobs) {
         }
 
-        void run_frame();
+        void run_frame(const std::vector<Task *> &tasks_ref) const;
 
-        void add_job(int16_t task_id, time::TimeDuration exec_tm) {
-            _jobs.emplace_back(task_id, exec_tm);
+        [[nodiscard]] std::string to_string() const {
+            std::string result;
+            for (auto &fj: _jobs) {
+                result += fj.to_string() + "\n";
+            }
+            return result;
         }
 
     private:
         // Each frame would remember which particular tasks it needs to run.
+        const std::vector<FrameJob> _jobs;
+    };
+
+    class FrameContainer {
+    public:
+        explicit FrameContainer(const std::vector<Task *> &tasks_ref, const std::vector<Frame> &&frames,
+                                time::TimeDuration frame_tm_dur)
+            : _frames(frames), _tasks_ref(const_cast<std::vector<Task *> &>(tasks_ref)),
+              _frame_tm_dur(frame_tm_dur) {
+        }
+
+        void run_frame(size_t k) {
+            if (k >= _frames.size()) {
+                throw std::out_of_range("[FrameContainer::run_frame] Index out of range");
+            }
+            _frames[k].run_frame(_tasks_ref);
+        }
+
+        [[nodiscard]] std::string to_string() const {
+            std::string result;
+            for (size_t i = 0; i < _frames.size(); ++i) {
+                result += "Frame " + std::to_string(i) + ":\n";
+                result += _frames[i].to_string() + "\n";
+            }
+            return result;
+        }
+
+        [[nodiscard]] const Frame &get_kth_frame(size_t k) const {
+            if (k >= _frames.size()) {
+                throw std::out_of_range("[FrameContainer::get_kth_frame] Index out of range");
+            }
+            return _frames[k];
+        }
+
+        [[nodiscard]] size_t size() const noexcept { return _frames.size(); }
+        [[nodiscard]] bool empty() const noexcept { return _frames.empty(); }
+
+    private:
         const std::vector<Task *> &_tasks_ref;
-        std::vector<FrameJob> _jobs;
+        const std::vector<Frame> _frames;
+        time::TimeDuration _frame_tm_dur{time::ZERO_DURATION};
     };
 }
 
--- src/frame.cpp
+++ src/frame.cpp
@@ -5,19 +5,20 @@
 #include "rtss/task.h"
 
 namespace rtss {
-    void Frame::run_frame() {
+    void Frame::run_frame(const std::vector<Task *> &tasks_ref) const {
         if (_jobs.empty()) {
             throw std::runtime_error("[Frame::run_frame] No jobs to run in this frame.");
         }
+        std::cout << "---- Frame ----" << std::endl;
         for (const auto &job: _jobs) {
             Task *T;
             if (job.task_id == static_cast<int16_t>(TaskID::IDLE)) {
                 T = Task::Idle();
             } else {
-                T = _tasks_ref[job.task_id - 1]; // task_id starts from 1 so that 0 and -1 can be reserved.
+                T = tasks_ref[job.task_id - 1]; // task_id starts from 1 so that 0 and -1 can be reserved.
             }
             T->run_task(job.exec_tm);
-            std::cout << "\tT" << T->get_id() << " duration = " << rtss::time::toInt(job.exec_tm) << " ms" << std::endl;
+            std::cout << job.to_string() << std::endl;
         }
     }
 }
