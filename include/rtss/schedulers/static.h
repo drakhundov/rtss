081c306b49344019dc904c1a9bf95ef575ecf45b
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-12-10 21:14:57 +0300

    Static schedulers complete.

--- include/rtss/schedulers/static.h
+++ include/rtss/schedulers/static.h
@@ -25,13 +25,31 @@ namespace rtss::schedulers {
         explicit TableDrivenScheduler(std::vector<Task *> &tasks,
                                       TaskTable &task_tbl)
             : ClockBasedScheduler(tasks, task_tbl) {
+            if (task_tbl.scheduling_mode() != SchedulingMode::TASK_BASED) {
+                throw std::runtime_error("[CyclicExecutiveScheduler::CyclicExecutiveScheduler] TaskTable must be in TASK_BASED mode");
+            }
         }
 
         void run_scheduler(size_t nperiods) override;
     };
 
     class CyclicExecutiveScheduler : public ClockBasedScheduler {
+    public:
+        explicit CyclicExecutiveScheduler(std::vector<Task *> tasks,
+                                          TaskTable &task_tbl,
+                                          int frame_sz)
+            : ClockBasedScheduler(tasks, task_tbl), frame_sz(frame_sz) {
+            if (task_tbl.scheduling_mode() != SchedulingMode::FRAME_BASED) {
+                throw std::runtime_error("[CyclicExecutiveScheduler::CyclicExecutiveScheduler] TaskTable must be in FRAME_BASED mode");
+            }
+        }
 
+        // TODO: constructor that determines frame size automatically.
+
+        void run_scheduler(size_t nperiods) override;
+
+    private:
+        int frame_sz{0};
     };
 }
 
